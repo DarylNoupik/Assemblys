e2f6aa4df091cab45fcc89b96513b48a1a03a077
Daryl <darylnoupik@gmail.com>
2020-11-24 09:16:51 +0100

    Ajoutez des fichiers projet.

---
+++ dll_point_Core/CLpoint.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dll_point_Core
+{
+    class CLpoint
+    {
+        private static int initialse { get => initialse; set initialse= value; } = 0;
+        private double _x
+        {
+            get => _x;
+            set
+            {
+                if (_x >= 0) { _x = value; }
+
+            }
+        }
+        private double _y
+        {
+            get => _y;
+            set { if (_y >= 0) { _y = value; } }
+        }
+
+        public CLpoint()
+        {
+            this._x = 0;
+            this._y = 0;
+            int id = 0;
+        }
+
+        public CLpoint(double x, double y)
+        {
+            this._x = x;
+            this._y = y;
+            int id = 0;
+        }
+
+        public void init_point (double x ,double y)
+        {
+            
+            
+            if (new CLpoint())
+            {
+                initialse++;
+                CLpoint.initialse = initialse;
+            }else if(new CLpoint(x,y))
+            {
+                 initialse++;
+            CLpoint.initialse = initialse;
+               
+            }
+        }
+
+    }
+}
---
+++ eXia_A2_sln_WS1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eXia_A2_sln_WS1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dll_pointCore.CLpoint point1;
+            dll_pointCore.CLpoint point2;
+            dll_pointCore.CLvoyage voyage;
+            List<dll_pointCore.CLpoint> etapes;
+
+            point1 = new dll_pointCore.CLpoint(1, 1);
+            point2 = new dll_pointCore.CLpoint(2, 2);
+            etapes = new List<dll_pointCore.CLpoint>(2);
+
+            etapes.Add(point1);
+            etapes.Add(point2);
+
+            voyage = new dll_pointCore.CLvoyage(etapes);
+            affichage(voyage);
+
+            voyage.delPoint(point2);
+            affichage(voyage);
+
+            voyage.addPoint(new dll_pointCore.CLpoint());
+            affichage(voyage);
+
+            Console.WriteLine("Distance du voyage : {0}", voyage.distanceVoyage());
+            Console.Read();
+        }
+        static void affichage(dll_pointCore.CLvoyage voyage)
+        {
+            int i;
+            for (i = 0; i < voyage.getEtape().Count(); i++)
+            {
+                Console.WriteLine("Point  : {0} - {1} - {2}", voyage.getEtape()[i].getID(), voyage.getEtape()[i].getX(), voyage.getEtape()[i].getY());
+            }
+            Console.WriteLine("-------------------");
+        }
+    }
+}
+
+    }
+}
